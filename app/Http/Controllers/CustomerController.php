5cbcbdd739435b25f1d57570bd51402539b0c51b
Siddharth <siddharth@marmeto.com>
2020-05-01 16:34:19 +0530

    Fixes

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -15,7 +15,7 @@ class CustomerController extends Controller {
         $store = Store::where('id', $id)->first();
         if($request->expectsJson()) {
             if($store !== NULL && $store->count() > 0) {
-                $customers = Customers::where('store_id', $id)->select('id', 'name')->get()->toArray();
+                $customers = Customers::where('store_id', $id)->select('id', 'first_name', 'last_name')->get()->toArray();
                 return response()->json(['status' => true, 'customers' => $customers], 200);
             } else return response()->json(['status' => false, 'message' => 'Store Not Found !'], 200);
         }
