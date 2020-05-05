fa04d012c3d7201adc595bf5c5498232fcbad4a5
Siddharth <siddharth@marmeto.com>
2020-05-05 18:09:54 +0530

    Customers api fix

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Customers;
 use App\Models\Store;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerController extends Controller {
     public function __construct() {
         $this->middleware('auth:api'); 
     }
 
-    public function show($id, Request $request) {
-        $store = Store::where('id', $id)->first();
+    public function index(Request $request) {
+        $store = Store::where('id', Auth::user()->store_id)->first();
         if($request->expectsJson()) {
             if($store !== NULL && $store->count() > 0) {
-                $customers = Customers::where('store_id', $id)->select('id', 'first_name', 'last_name')->get()->toArray();
+                $customers = Customers::where('store_id', Auth::user()->store_id)->select('id', 'first_name', 'last_name')->get()->toArray();
                 return response()->json(['status' => true, 'customers' => $customers], 200);
             } else return response()->json(['status' => false, 'message' => 'Store Not Found !'], 200);
         }
